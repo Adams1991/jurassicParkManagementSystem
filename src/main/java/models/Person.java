18b0af276399a420dc2d102bf67a9628b100b301
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-19 14:53:50 +0100

    created person class

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,4 +1,54 @@
 package models;
 
-public class Person {
+import javax.persistence.*;
+
+public abstract class Person {
+    private int id;
+    private String name;
+    private int nutritionalValue;
+    private Park park;
+
+    public Person(){}
+
+    public Person(String name, int nutritionalValue, Park park) {
+        this.name = name;
+        this.nutritionalValue = nutritionalValue;
+        this.park = park;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNutritionalValue() {
+        return nutritionalValue;
+    }
+
+    public void setNutritionalValue(int nutritionalValue) {
+        this.nutritionalValue = nutritionalValue;
+    }
+    @ManyToOne
+    @JoinColumn(name="park_id", nullable=false)
+    public Park getPark() {
+        return park;
+    }
+
+    public void setPark(Park park) {
+        this.park = park;
+    }
 }
