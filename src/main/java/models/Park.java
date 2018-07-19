3b096b6da9af1ad6ddb40968538b4e14ce479e7d
Adams1991 <adams121@hotmail.co.uk>
2018-07-19 14:28:17 +0100

    park model setup started testing to come

--- src/main/java/models/Park.java
+++ src/main/java/models/Park.java
@@ -1,4 +1,61 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Park {
+    private int id;
+    private String name;
+    private ArrayList<Staff> staff;
+    private ArrayList<Visitor> visitors;
+    private ArrayList<Paddock> paddocks;
+
+    public Park() {
+    }
+
+    public Park(String name, ArrayList<Staff> staff, ArrayList<Visitor> visitors, ArrayList<Paddock> paddocks) {
+        this.name = name;
+        this.staff = staff;
+        this.visitors = visitors;
+        this.paddocks = paddocks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Staff> getStaff() {
+        return staff;
+    }
+
+    public void setStaff(ArrayList<Staff> staff) {
+        this.staff = staff;
+    }
+
+    public ArrayList<Visitor> getVisitors() {
+        return visitors;
+    }
+
+    public void setVisitors(ArrayList<Visitor> visitors) {
+        this.visitors = visitors;
+    }
+
+    public ArrayList<Paddock> getPaddocks() {
+        return paddocks;
+    }
+
+    public void setPaddocks(ArrayList<Paddock> paddocks) {
+        this.paddocks = paddocks;
+    }
 }
---
+++ src/test/java/models/ParkTest.java
@@ -0,0 +1,48 @@
+package models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParkTest {
+
+    Park park;
+
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getStaff() {
+    }
+
+    @Test
+    public void setStaff() {
+    }
+
+    @Test
+    public void getVisitors() {
+    }
+
+    @Test
+    public void setVisitors() {
+    }
+
+    @Test
+    public void getPaddocks() {
+    }
+
+    @Test
+    public void setPaddocks() {
+    }
+}
