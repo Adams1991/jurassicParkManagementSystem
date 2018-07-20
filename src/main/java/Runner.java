4c1fa93b78c5ad481b268194ee1d8086fc6bd59a
Adams1991 <adams121@hotmail.co.uk>
2018-07-20 12:24:22 +0100

    crud function for park and staff tested in runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,35 +1,68 @@
 import db.DBHelper;
 import models.*;
 
+import java.util.List;
+
 public class Runner {
     public static void main(String[] args) {
 
-//        DBHelper.deleteAll(Park.class);
-//        DBHelper.deleteAll(Staff.class);
-//        DBHelper.deleteAll(Paddock.class);
-//        DBHelper.deleteAll(Carnivore.class);
-//        DBHelper.deleteAll(Herbivore.class);
-//        DBHelper.deleteAll(Visitor.class);
 
+
+        //CRUD Functions for Park
         Park park = new Park("Jurassic Park");
         DBHelper.save(park);
 
+        Park park2 = new Park("Jurassic Park");
+        DBHelper.save(park2);
+
+        park.setName("Disney");
+        DBHelper.update(park);
+
+        List<Park> allParks = DBHelper.getAll(Park.class);
+
+        DBHelper.delete(park2);
+
+        List<Park> allParks2 = DBHelper.getAll(Park.class);
+
+        DBHelper.find(Park.class, 1);
+
+        //CRUD Function for Staff
+
         Staff staffMember = new Staff("Pavel Cody", RoleType.GAMEKEEPER, park);
         DBHelper.save(staffMember);
+        staffMember.setJob(RoleType.SECURITYGUARD);
+        DBHelper.update(staffMember);
 
+        Staff staffMember2 =  new Staff("Pavel Cody", RoleType.GAMEKEEPER, park);;
+        DBHelper.save(staffMember2);
+
+        List<Staff> allStaff = DBHelper.getAll(Staff.class);
+
+        DBHelper.delete(staffMember2);
+
+        List<Staff> allStaff2 = DBHelper.getAll(Staff.class);
+
+
+        //CRUD Function for Paddock
         Paddock tRexEnclosure = new Paddock(park, "Tyranasaurs");
         DBHelper.save(tRexEnclosure);
+        tRexEnclosure.setName("Bla");
+        DBHelper.update(tRexEnclosure);
+
 
         Paddock grassEateres = new Paddock(park, "Field");
         DBHelper.save(grassEateres);
+        DBHelper.delete(grassEateres);
 
         Carnivore tRex = new Carnivore("Lizard King", 100, SpeciesType.TREX, tRexEnclosure);
         DBHelper.save(tRex);
 
-        Herbivore dippy = new Herbivore("Long Tail", 100, SpeciesType.DIPLODOCUS, grassEateres);
+        Herbivore dippy = new Herbivore("Long Tail", 100, SpeciesType.DIPLODOCUS, tRexEnclosure);
         DBHelper.save(dippy);
 
         Visitor visitor = new Visitor("Dan", 26, 182, park);
         DBHelper.save(visitor);
+
+
     }
 }
