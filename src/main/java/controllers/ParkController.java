dd983528849d820e86d76b564151d12a7135ff16
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-20 12:35:19 +0100

    added all controller packages

--- src/main/java/controllers/ParkController.java
+++ src/main/java/controllers/ParkController.java
@@ -1,4 +1,96 @@
 package controllers;
 
+import db.DBHelper;
+import models.Park;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class ParkController {
+    public ParkController() {
+        this.setupEndPoints();
+    }
+
+    private void setupEndPoints() {
+
+
+        //Read
+
+        get("/parks", (req, res) -> {
+            Map<String, Object> model = new HashMap();
+            model.put("template", "templates/parks/index.vtl");
+
+            List<Park> parks = DBHelper.getAll(Park.class);
+            model.put("parks", parks);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+        //CREATE GET
+
+        get("/parks/new", (req, res) -> {
+            HashMap<String, Object> model = new HashMap();
+
+            model.put("template", "templates/parks/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+        //CREATE POST
+
+        post("/parks", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = req.queryParams("name");
+            Park park = new Park(name);
+            DBHelper.save(park);
+
+            res.redirect("/parks");
+            return null;
+
+        }, new VelocityTemplateEngine());
+
+        // DELETE
+        post("/parks/:id/delete", (req, res) -> {
+            int id = Integer.parseInt(req.params(":id"));
+            Park park = DBHelper.find(Park.class, id);
+            DBHelper.delete(park);
+            res.redirect("/parks");
+            return null;
+        }, new VelocityTemplateEngine());
+
+        //UPDATE GET
+        get("/parks/:id/edit", (req, res) -> {
+            HashMap<String, Object> model = new HashMap();
+
+            int id = Integer.parseInt(req.params(":id"));
+            Park park = DBHelper.find(Park.class, id);
+            model.put("park", park);
+            model.put("template", "templates/parks/edit.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+        //UPDATE POST
+        post("parks/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = req.queryParams("name");
+
+            int id = Integer.parseInt(req.params(":id"));
+            Park park = DBHelper.find(Park.class, id);
+            park.setName(name);
+            DBHelper.update(park);
+
+            res.redirect("/parks");
+            return null;
+        }, new VelocityTemplateEngine());
+
+    }
 }
+
