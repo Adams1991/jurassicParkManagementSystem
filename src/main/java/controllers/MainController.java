05c7673ad9ba74a42951be6a4a6076266ced47dc
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-24 19:14:18 +0100

    css issues

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -13,9 +13,9 @@ import static spark.Spark.staticFileLocation;
 public class MainController {
     public static void main(String[] args) {
 
-        staticFileLocation("/public");
 
         Seeds.seedData();
+        staticFileLocation("/public");
 
         ParkController parkController = new ParkController();
         PaddockController paddockContoller = new PaddockController();
@@ -25,8 +25,8 @@ public class MainController {
         CarnivoreController carnivoreController = new CarnivoreController();
         HerbivoreController herbivoreController = new HerbivoreController();
         AttractionController attractionController = new AttractionController();
-
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+
 
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap();
